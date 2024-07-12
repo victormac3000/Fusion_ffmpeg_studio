4da90128db0c6bfd9eb7ad9f819eb3ad57058ac4
Víctor Martínez <25689551+victormac3000@users.noreply.github.com>
2024-07-12 19:14:22 +0200

    Dialogs now do not leak memory

--- utils/dialogs.cpp
+++ utils/dialogs.cpp
@@ -3,31 +3,43 @@
 void Dialogs::ok(QString userMsg, QString logMsg)
 {
     qDebug() << "Ok dialog displayed:" << (logMsg.isEmpty() ? userMsg : logMsg);
-    QMessageBox *box = new QMessageBox;
-    box->setWindowTitle("Information");
-    box->setIcon(QMessageBox::Information);
-    box->setText(userMsg);
-    box->open();
+    QMessageBox box;
+    box.setWindowTitle("Information");
+    box.setIcon(QMessageBox::Information);
+    box.setText(userMsg);
+    box.exec();
 }
 
 void Dialogs::warning(QString userMsg, QString logMsg)
 {
     qWarning() << "Warning dialog displayed:" << (logMsg.isEmpty() ? userMsg : logMsg);
-    QMessageBox *box = new QMessageBox;
-    box->setWindowTitle("Concerning error");
-    box->setIcon(QMessageBox::Warning);
-    box->setText(userMsg);
-    box->open();
+    QMessageBox box;
+    box.setWindowTitle("Concerning error");
+    box.setIcon(QMessageBox::Warning);
+    box.setText(userMsg);
+    box.exec();
 }
 
 void Dialogs::critical(QString userMsg, QString logMsg)
 {
     qCritical() << "Critical dialog displayed:" << (logMsg.isEmpty() ? userMsg : logMsg);
-    QMessageBox *box = new QMessageBox;
-    box->setWindowTitle("Critical error");
-    box->setIcon(QMessageBox::Critical);
-    box->setText(userMsg);
-    box->exec();
+    QMessageBox box;
+    box.setWindowTitle("Critical error");
+    box.setIcon(QMessageBox::Critical);
+    box.setText(userMsg);
+    box.exec();
+    QCoreApplication::exit(ERROR_CRITICAL);
+    exit(ERROR_CRITICAL);
+}
+
+void Dialogs::criticalStart(QString logMsg)
+{
+    qCritical() << "Critical start error" << logMsg;
+    QMessageBox box;
+    box.setWindowTitle("Critical error");
+    box.setIcon(QMessageBox::Critical);
+    box.setText("There was an internal error starting the application\nCheck the logs for more information");
+    box.exec();
     QCoreApplication::exit(ERROR_CRITICAL);
     exit(ERROR_CRITICAL);
 }
--- utils/dialogs.h
+++ utils/dialogs.h
@@ -15,6 +15,7 @@ public:
     static void ok(QString userMsg, QString logMsg = "");
     static void warning(QString userMsg, QString logMsg = "");
     static void critical(QString userMsg, QString logMsg = "");
+    static void criticalStart(QString logMsg);
 };
 
 #endif // DIALOGS_H
